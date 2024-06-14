a4c5557d6945f8da7e85e4abaeaf80b058cbf73d
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-06-15 01:53:16 +0500

    added to js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let map = document.querySelector("#map");
 setup();
 
 function setup() {
-    map.innerHTMLL="";
+    map.innerHTML="";
     for (let i = 0; i < city.length; i++) {
         for (let j = 0; j <city[0].length; j++) {
             console.log(city[i][j]);
