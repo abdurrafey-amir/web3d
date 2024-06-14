41823b4abd45779d8f351b15e60267464d0bae7c
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-06-15 01:54:37 +0500

    js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function setup() {
     map.innerHTML="";
     for (let i = 0; i < city.length; i++) {
         for (let j = 0; j <city[0].length; j++) {
+            let x = (i * 5) - (2.5 * (city.length - 1))
+            let z = -j * 5 + (2.5 * (city.length - 1))
+            let val = city[i][j];
+            addBuilding(x, z, val);
             console.log(city[i][j]);
         }
     }
